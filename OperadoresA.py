8dbe9bcd1dd9e00ffbb87efbc6d2649d1f74f19e
Angel Tadeo De Leon Ceniceros <a.22308051281165@cbtis128.edu.mx>
2024-09-20 09:33:31 -0600

    Add files via upload

---
+++ OperadoresA.py
@@ -0,0 +1,50 @@
+print("Act 9 Clases v3")
+print("Angel Tadeo Mat: 22308051281165")
+# Zona de class
+class Operadores1165:
+    # Zona de funciones
+    # Suma
+    def suma1165(self,A,T):
+        s1165=A+T
+        print(f"La suma de {A} + {T} = {s1165}")
+    # Resta
+    def resta1165(self,A,T):
+        r1165=A-T
+        print(f"La resta de {A} - {T} = {r1165}")    
+    # Multiplicacion
+    def multi1165(self,A,T):
+        m1165=A*T
+        print(f"La multiplicacion de {A} x {T} = {m1165}")    
+    # Division
+    def div1165(self,A,T):
+        d1165=A/T
+        print(f"La division de {A} / {T} = {d1165}")
+    # Modulo
+    def mod1165(self,A,T):
+        mo1165=A%T
+        print(f"El modulo de {A} % {T} = {mo1165}")
+    # Exponente
+    def expo1165(self,A,T):
+        e1165=A**T
+        print(f"El exponente de {A} ** {T} = {e1165}")
+    # Cociente
+    def coci1165(self,A,T):
+        c1165=A//T
+        print(f"El cociente de {A} // {T} = {c1165}")    
+# Zona de creacion del objeto
+operatadeo=Operadores1165()
+# Zona de uso de objetos
+print("Funcion suma:")
+operatadeo.suma1165(10,3)
+print("Funcion resta:")
+operatadeo.resta1165(10,3)
+print("Funcion multiplicacion:")
+operatadeo.multi1165(10,3)
+print("Funcion division:")
+operatadeo.div1165(10,3)
+print("Funcion modulo:")
+operatadeo.mod1165(10,3)
+print("Funcion exponente:")
+operatadeo.expo1165(10,3) 
+print("Funcion cociente:")
+operatadeo.coci1165(10,3)
